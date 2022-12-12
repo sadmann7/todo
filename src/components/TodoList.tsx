59b858a47f17d43fce82dd078c5c7c54dd705cb2
sadmann7 <sadmann898@gmail.com>
2022-12-13 01:19:05 +0600

    :zap: Add: editTodo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -85,8 +85,10 @@ const TodoList = () => {
   }, [setFocus, showInput]);
 
   // AutoAnimate
-  const [todosRef] = useAutoAnimate<HTMLDivElement>();
+  const [todosRef] = useAutoAnimate<HTMLUListElement>();
   const [formRef] = useAutoAnimate<HTMLFormElement>();
+
+  // AddTodo keyboard shortcut
 
   return (
     <section
@@ -96,13 +98,13 @@ const TodoList = () => {
       {status === "loading" ? (
         <p className="mt-5 text-sm md:text-base">Loading todos...</p>
       ) : (
-        <div className="mt-5 grid gap-5" ref={todosRef}>
+        <ul className="mt-5 grid gap-5" ref={todosRef}>
           {todos?.map((todo) => (
             <Fragment key={todo.id}>
               <TodoItem todo={todo} deleteTodo={deleteTodo} />
             </Fragment>
           ))}
-        </div>
+        </ul>
       )}
       <form
         aria-label="todo_form"
@@ -150,7 +152,7 @@ const TodoList = () => {
             <div
               role="button"
               aria-label="add todo"
-              className="flex cursor-pointer items-center space-x-2"
+              className="flex cursor-pointer items-center space-x-2 transition-opacity hover:opacity-80 active:opacity-100"
               onClick={() => setShowInput(true)}
             >
               <PlusIcon
@@ -186,6 +188,7 @@ type TodoItemProps = {
 const TodoItem = ({ todo, deleteTodo }: TodoItemProps) => {
   const [isHoverd, setIsHoverd] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
+  const [todoLabel, setTodoLabel] = useState(todo.label);
 
   // trpc
   const utils = trpc.useContext();
@@ -205,48 +208,105 @@ const TodoItem = ({ todo, deleteTodo }: TodoItemProps) => {
             : todo
         )
       );
+      toast.success("Todo updated.");
     },
   });
 
   return (
-    <div
-      className="border-b-1 flex cursor-pointer items-center justify-between gap-2 border-b-2 border-b-gray-700 pb-2.5"
+    <li
+      className={`border-b-1 flex cursor-pointer border-b-2 border-b-gray-700 pb-2.5 ${
+        isEditing ? "flex-col" : "flex-row items-center justify-between gap-2"
+      }`}
       onMouseEnter={() => setIsHoverd(true)}
       onMouseLeave={() => setIsHoverd(false)}
+      onDoubleClick={() => setIsEditing(!isEditing)}
     >
-      <div className="flex items-center gap-5">
-        <input
-          aria-label="check todo"
-          id="progress"
-          type="checkbox"
-          className="focus:ring-none cursor-pointer rounded-full border-2 bg-transparent"
-          checked={todo.completed}
-          onChange={(e) => {
-            const checked = e.currentTarget.checked;
-            editTodo({
-              id: todo.id,
-              data: { completed: checked },
-            });
-          }}
-          autoFocus={isEditing}
-        />
-        <p
-          className={`${
-            todo.completed && "line-through"
-          } text-xs line-clamp-1 md:text-sm`}
-          onDoubleClick={() => {
-            setIsEditing(true);
-          }}
-        >
-          {todo.label}
-        </p>
-      </div>
-      {isHoverd && (
+      {isEditing ? (
+        <Fragment>
+          <input
+            id="editableTodo"
+            type="text"
+            className="w-full rounded-md bg-black/50 px-4"
+            onChange={(e) => {
+              setTodoLabel(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && todoLabel.length > 0) {
+                editTodo({
+                  id: todo.id,
+                  data: { label: todoLabel },
+                });
+                setIsEditing(false);
+              }
+            }}
+            defaultValue={todoLabel}
+            autoFocus={isEditing}
+          />
+          <div className="mt-3 flex">
+            <div className="ml-auto flex items-center space-x-3">
+              <div
+                role="button"
+                aria-label="cancel"
+                className="cursor-pointer rounded-md bg-red-500 px-4 py-1.5 text-xs font-medium transition-opacity hover:bg-opacity-75 active:opacity-100 md:text-sm"
+                onClick={() => setIsEditing(!isEditing)}
+              >
+                Cancel
+              </div>
+              <button
+                type="submit"
+                aria-label="add todo"
+                className={`${
+                  todoLabel.length > 0
+                    ? "cursor-pointer transition-opacity hover:bg-opacity-75 active:opacity-100"
+                    : "cursor-not-allowed"
+                } rounded-md bg-green-500 px-4 py-1.5 text-xs font-medium md:text-sm`}
+                onClick={() => {
+                  editTodo({
+                    id: todo.id,
+                    data: { label: todoLabel },
+                  });
+                  setIsEditing(false);
+                }}
+                disabled={todoLabel.length <= 0}
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </Fragment>
+      ) : (
+        <div className="flex items-center gap-5">
+          <input
+            aria-label="check todo"
+            id="progress"
+            type="checkbox"
+            className="focus:ring-none cursor-pointer rounded-full border-2 bg-transparent"
+            checked={todo.completed}
+            onChange={(e) => {
+              const checked = e.currentTarget.checked;
+              editTodo({
+                id: todo.id,
+                data: { completed: checked },
+              });
+            }}
+            autoFocus={isEditing}
+          />
+          <p
+            className={`${
+              todo.completed && "line-through"
+            } text-xs line-clamp-1 md:text-sm`}
+          >
+            {todoLabel}
+          </p>
+        </div>
+      )}
+      {isHoverd && !isEditing && (
         <div className="flex items-center gap-2">
           <PencilSquareIcon
             role="button"
             aria-label="edit todo"
             className="aspect-square w-5 text-gray-400 transition-colors hover:text-gray-300 active:text-gray-400"
+            onClick={() => setIsEditing(true)}
           />
           <TrashIcon
             role="button"
@@ -256,6 +316,6 @@ const TodoItem = ({ todo, deleteTodo }: TodoItemProps) => {
           />
         </div>
       )}
-    </div>
+    </li>
   );
 };
